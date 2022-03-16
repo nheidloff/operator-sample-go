b488b6aa7e5af92adc63b5fe5b01ef0a5f3ca59b
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2022-03-16 07:50:51 +0100

    db change

--- operator-database/controllers/database_controller.go
+++ operator-database/controllers/database_controller.go
@@ -22,7 +22,7 @@ type DatabaseReconciler struct {
 func (r *DatabaseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
-	log.Info("Reconcile loop started")
+	log.Info("Reconcile started")
 
 	return ctrl.Result{}, nil
 }
