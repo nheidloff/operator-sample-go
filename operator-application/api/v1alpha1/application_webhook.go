7913450541045d8e9ccdaac3a030a93e68bed9c7
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2022-03-22 20:56:09 +0100

    trying

--- operator-application/api/v1alpha1/application_webhook.go
+++ operator-application/api/v1alpha1/application_webhook.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -23,7 +7,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
-// log is for logging in this package.
 var applicationlog = logf.Log.WithName("application-resource")
 
 func (r *Application) SetupWebhookWithManager(mgr ctrl.Manager) error {
@@ -32,9 +15,7 @@ func (r *Application) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-//+kubebuilder:webhook:path=/mutate-application-sample-ibm-com-v1alpha1-application,mutating=true,failurePolicy=fail,sideEffects=None,groups=application.sample.ibm.com,resources=applications,verbs=create;update,versions=v1alpha1,name=mapplication.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/mutate-application-sample-ibm-com-v1alpha1-application,mutating=true,failurePolicy=fail,sideEffects=None,groups=application.sample.ibm.com,resources=applications,verbs=create;update,versions=v1alpha1,name=mapplication.kb.io,admissionReviewVersions={v1alpha1,v1beta1}
 
 var _ webhook.Defaulter = &Application{}
 
@@ -48,7 +29,7 @@ func (r *Application) Default() {
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:path=/validate-application-sample-ibm-com-v1alpha1-application,mutating=false,failurePolicy=fail,sideEffects=None,groups=application.sample.ibm.com,resources=applications,verbs=create;update,versions=v1alpha1,name=vapplication.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-application-sample-ibm-com-v1alpha1-application,mutating=false,failurePolicy=fail,sideEffects=None,groups=application.sample.ibm.com,resources=applications,verbs=create;update,versions=v1alpha1,name=vapplication.kb.io,admissionReviewVersions={v1alpha1,v1beta1}
 
 var _ webhook.Validator = &Application{}
 
